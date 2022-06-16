5a5946ddcd429378c44d6ecb2acc129f340b902a
named.josie <named.josie@gmail.com>
2022-06-15 22:49:35 -0400

    updated header and added icons

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Header.css'
+import MenuIcon from '@mui/icons-material/MenuSharp';
+import SearchIcon from "@mui/icons-material/Search";
+import VideoCallIcon from "@mui/icons-material/VideoCall"
+import AppsIcon from "@mui/icons-material/Apps"
+import  NotificationsIcon  from '@mui/icons-material/Notifications';
+import { Avatar } from '@mui/material';
 
 export default function Header() {
-      return (
-        <header>
-        <Link to="/">
-        <img  src='https://1000logos.net/wp-content/uploads/2017/05/YouTube-Logo-2015.jpg' alt="image" />
-        </Link>
-        </header>
-      );
-    };
+  return (
+    <div className='header'>
+      <div className='header-left'>
+      <MenuIcon />
+      <img
+        className='logo'
+        src='https://www.citypng.com/public/uploads/small/51609620783uc927um4fsecn8ujdlnfdqe6vno4kjnxyb3hyv3yatqwsg5kyuwz5ukduz8anllw7azyduy8fpjih9hzg2u61lt84vn1iqhzfb55.png'
+        alt='youtube'
+      />
+      </div>
+      <div className='input-container'>
+      <input type="text" placeholder='Search' />
+      <SearchIcon/>
+      </div>
+      <div className='special-icons'>
+      <VideoCallIcon/>
+      <AppsIcon/>
+      <NotificationsIcon/>
+      <Avatar/>
+      </div>
+    </div>
+  );
+}
